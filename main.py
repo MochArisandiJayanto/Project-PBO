5d470d338338660cd35fa48eace029ed8c92afb6
MochArisandiJayanto <diajaya0@gmail.com>
2020-12-08 22:49:13 +0700

    Menambah code menampilkan akun

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-  
+import sqlite3
+
+class Bank:
+
+    def melihatSaldo():
+        con=sqlite3.connect("data.db")
+        cur =con.cursor()
+        cur.execute("SELECT Nama,[Jumlah Saldo] From Akun")
+        rows =cur.fetchall()
+        con.close()
+        return rows
+
+    def melihatAkun():
+        con=sqlite3.connect("data.db")
+        cur =con.cursor()
+        cur.execute("SELECT Nama From Akun")
+        rows =cur.fetchall()
+        con.close()
+        return rows
+
 def intro():
     print("\t\t\t\t**********************")
     print("\t\t\t\tSISTEM MANAJEMEN BANK")
@@ -19,23 +38,27 @@ while ch != 8:
     print("\t\t1. Membuat Akun Baru")
     print("\t\t2. Jumlah Saldo")
     print("\t\t3. Menarik Saldo")
-    print("\t\t4. Cek Sisa Saldo")
+    print("\t\t4. Transfer")
     print("\t\t5. Edit Akun")
-    print("\t\t6. EXIT")
-    ch = int(input("\t\tMasukkan Pilihan Anda (1-6) :  "))
+    print("\t\t6. Melihat Akun")
+    print("\t\t7. Exit")
+    ch = int(input("\t\tMasukkan Pilihan Anda (1-7) :  "))
     #system("cls");
     
-    if ch == '1':
+    if ch == 1:
         print("Membuat Akun")
-    elif ch =='2':
+    elif ch == 2:
         print("Jumlah Saldo")
-    elif ch == '3':
+        print(Bank.melihatSaldo())
+    elif ch == 3:
         print("Menarik saldo")
-    elif ch == '4':
-        print("Sisa Saldo")
-    elif ch == '5':
+    elif ch == 4:
+        print("Transfer")
+    elif ch == 5:
         print("Edit Akun")
-    elif ch == '6':
+    elif ch == 6:
+        print(Bank.melihatAkun())
+    elif ch == 7:
         print("\tTerimakasih Sudah Menggunakan SISTEM MANAJEMEN BANK")
         break
     else :
